2746ee0b14e79699ef2971178ff4a6e74f3988fe
LegosAndStuff-Dev <masonnorberg@midco.net>
2021-12-28 10:21:18 -0600

    added top.gg stuff

--- Dinosaur.py
+++ Dinosaur.py
@@ -351,6 +351,427 @@ async def on_guild_remove(guild):
 
     await join.send(embed=embed)
 
+client.topgg_webhook = topgg.WebhookManager(client).dbl_webhook("/dblwebhook", "password")
+client.topgg_webhook.run(5000)
+
+@client.event
+async def on_dbl_vote(data):
+    if data["type"] == "test":
+        guild = client.get_guild(840354954074128405)
+
+        votechannel = discord.utils.get(guild.channels, name="vote-logs")
+        
+        user = 638092957756555291
+
+        user = client.get_user(user)
+
+        conn = sqlite3.connect("vote.db")
+        c = conn.cursor()
+
+        c.execute(f"SELECT * FROM vote WHERE user_ID={user.id}")
+
+        items = c.fetchall()
+        none = str(items)
+
+        if none == "[]":
+            c.execute(f"INSERT INTO vote VALUES ({user.id}, 1)")
+
+            conn.commit()
+
+        else:
+            for item in items:
+                vote = int(item[1])
+
+            vote = vote + 1
+
+            c.execute(f"""UPDATE vote SET votes = {vote}
+                        WHERE user_ID = {user.id}  
+                    """)
+
+            conn.commit()
+            conn.close()
+
+        embed: discord.Embed = discord.Embed(
+            title=f"{user.name} Voted!",
+            description=f"ID - {user.id}",
+            color=discord.Color.dark_green()
+        )
+        embed.set_footer(text=f"This user has voted {vote} times")
+
+        await votechannel.send(embed=embed)
+
+        if none =="[]":
+            embed: discord.Embed = discord.Embed(
+                title="Thanks for voting!",
+                description=f"You have voted 1 time so far!",
+                color=discord.Color.dark_green()
+            )
+            try:
+                await user.send(embed=embed)
+
+            except:
+                pass
+
+        else:
+            embed: discord.Embed = discord.Embed(
+                title="Thanks for voting!",
+                description=f"You have voted {vote} times so far!",
+                color=discord.Color.dark_green()
+            )
+
+            try:
+                await user.send(embed=embed)
+
+            except:
+                pass
+
+    else:
+        guild = client.get_guild(840354954074128405)
+
+        votechannel = discord.utils.get(guild.channels, name="vote-logs")
+        
+        user = 638092957756555291
+
+        user = client.get_user(user)
+
+        conn = sqlite3.connect("vote.db")
+        c = conn.cursor()
+
+        c.execute(f"SELECT * FROM vote WHERE user_ID={user.id}")
+
+        items = c.fetchall()
+        none = str(items)
+
+        if none == "[]":
+            c.execute(f"INSERT INTO vote VALUES ({user.id}, 1)")
+
+            conn.commit()
+
+        else:
+            for item in items:
+                vote = int(item[1])
+
+            vote = vote + 1
+
+            c.execute(f"""UPDATE vote SET votes = {vote}
+                        WHERE user_ID = {user.id}  
+                    """)
+
+            conn.commit()
+            conn.close()
+
+        embed: discord.Embed = discord.Embed(
+            title=f"{user.name} Voted!",
+            description=f"ID - {user.id}",
+            color=discord.Color.green()
+        )
+        embed.set_footer(text=f"This user has voted {vote} times")
+
+        await votechannel.send(embed=embed)
+
+        if none =="[]":
+            embed: discord.Embed = discord.Embed(
+                title="Thanks for voting!",
+                description=f"You have voted 1 time so far!",
+                color=discord.Color.green()
+            )
+            try:
+                await user.send(embed=embed)
+
+            except:
+                pass
+
+        else:
+            embed: discord.Embed = discord.Embed(
+                title="Thanks for voting!",
+                description=f"You have voted {vote} times so far!",
+                color=discord.Color.green()
+            )
+
+            try:
+                await user.send(embed=embed)
+
+            except:
+                pass
+
+@tasks.loop(minutes=2)
+async def check_data():
+    x = datetime.datetime.now()
+    m_d_y = x.strftime("%x")
+
+    date = m_d_y.split("/")
+
+    m = date[0]
+    d = date[1]
+    y = date[2]
+
+    if d == "1":
+        votechannel = client.get_channel(861773377742831656)
+
+        conn = sqlite3.connect("vote.db")
+        c = conn.cursor()
+
+        c.execute("SELECT * FROM vote ORDER BY votes DESC LIMIT 1 OFFSET 0")
+
+        items_1 = c.fetchall()
+        none_1 = str(items_1)
+
+        c.execute("SELECT * FROM vote ORDER BY votes DESC LIMIT 1 OFFSET 1")
+
+        items_2 = c.fetchall()
+        none_2 = str(items_2)
+
+        c.execute("SELECT * FROM vote ORDER BY votes DESC LIMIT 1 OFFSET 2")
+
+        items_3 = c.fetchall()
+        none_3 = str(items_3)
+
+        c.execute("SELECT * FROM vote ORDER BY votes DESC LIMIT 1 OFFSET 3")
+
+        items_4 = c.fetchall()
+        none_4 = str(items_4)
+
+        c.execute("SELECT * FROM vote ORDER BY votes DESC LIMIT 1 OFFSET 4")
+
+        items_5 = c.fetchall()
+        none_5 = str(items_5)
+
+        c.execute("SELECT * FROM vote ORDER BY votes DESC LIMIT 1 OFFSET 5")
+
+        items_6 = c.fetchall()
+        none_6 = str(items_6)
+
+        c.execute("SELECT * FROM vote ORDER BY votes DESC LIMIT 1 OFFSET 6")
+
+        items_7 = c.fetchall()
+        none_7 = str(items_7)
+
+        c.execute("SELECT * FROM vote ORDER BY votes DESC LIMIT 1 OFFSET 7")
+
+        items_8 = c.fetchall()
+        none_8 = str(items_8)
+
+        c.execute("SELECT * FROM vote ORDER BY votes DESC LIMIT 1 OFFSET 8")
+
+        items_9 = c.fetchall()
+        none_9 = str(items_9)
+
+        c.execute("SELECT * FROM vote ORDER BY votes DESC LIMIT 1 OFFSET 9")
+
+        items_10 = c.fetchall()
+        none_10 = str(items_10)
+
+        if none_1 == "[]":
+            user_1 = "Unavailable"
+            votes_1 = 0
+
+        else:
+            for item_1 in items_1:
+                user_1_id = int(item_1[0])
+                vote_1 = int(item_1[1])
+
+            user_1 = client.get_user(user_1_id)
+
+            if user_1 == None:
+                user_1 = user_1_id
+
+            else:
+                user_1 = user_1.name
+
+        if none_2 == "[]":
+            user_2 = "Unavailable"
+            vote_2 = 0
+
+        else:
+            for item in items_2:
+                user_2_id = int(item[0])
+                vote_2 = int(item[1])
+
+            user_2 = client.get_user(user_2_id)
+
+            if user_2 == None:
+                user_2 = user_2_id
+
+            else:
+                user_2 = user_2.name
+
+        if none_3 == "[]":
+            user_3 = "Unavailable"
+            vote_3 = 0
+
+        else:
+            for item in items_3:
+                user_3_id = int(item[0])
+                vote_3 = int(item[1])
+
+            user_3 = client.get_user(user_3_id)
+
+            if user_3 == None:
+                user_3 = user_3_id
+
+            else:
+                user_3 = user_3.name
+
+        if none_4 == "[]":
+            user_4 = "Unavailable"
+            vote_4 = 0
+
+        else:
+            for item in items_4:
+                user_4_id = int(item[0])
+                vote_4 = int(item[1])
+
+            user_4 = client.get_user(user_4_id)
+
+            if user_4 == None:
+                user_4 = user_4_id
+
+            else:
+                user_4 = user_4.name
+
+        if none_5 == "[]":
+            user_5 = "Unavailable"
+            vote_5 = 0
+
+        else:
+            for item in items_5:
+                user_5_id = int(item[0])
+                vote_5 = int(item[1])
+
+            user_5 = client.get_user(user_5_id)
+
+            if user_5 == None:
+                user_5 = user_5_id
+
+            else:
+                user_5 = user_5.name
+
+        if none_6 == "[]":
+            user_6 = "Unavailable"
+            vote_6 = 0
+
+        else:
+            for item in items_6:
+                user_6_id = int(item[0])
+                vote_6 = int(item[1])
+
+            user_6 = client.get_user(user_6_id)
+
+            if user_6 == None:
+                user_6 = user_6_id
+
+            else:
+                user_6 = user_6.name
+
+        if none_7 == "[]":
+            user_7 = "Unavailable"
+            vote_7 = 0
+
+        else:
+            for item in items_7:
+                user_7_id = int(item[0])
+                vote_7 = int(item[1])
+
+            user_7 = client.get_user(user_7_id)
+
+            if user_7 == None:
+                user_7 = user_7_id
+
+            else:
+                user_7 = user_7.name
+
+        if none_8 == "[]":
+            user_8 = "Unavailable"
+            vote_8 = 0
+
+        else:
+            for item in items_8:
+                user_8_id = int(item[0])
+                vote_8 = int(item[1])
+
+            user_8 = client.get_user(user_8_id)
+
+            if user_8 == None:
+                user_8 = user_8_id
+
+            else:
+                user_8 = user_8.name
+
+        if none_9 == "[]":
+            user_9 = "Unavailable"
+            vote_9 = 0
+
+        else:
+            for item in items_9:
+                user_9_id = int(item[0])
+                vote_9 = int(item[1])
+
+            user_9 = client.get_user(user_9_id)
+
+            if user_9 == None:
+                user_9 = user_9_id
+
+            else:
+                user_9 = user_9.name
+
+        if none_10 == "[]":
+            user_10 = "Unavailable"
+            vote_10 = 0
+
+        else:
+            for item in items_10:
+                user_10_id = int(item[0])
+                vote_10 = int(item[1])
+
+            user_10 = client.get_user(user_10_id)
+
+            if user_10 == None:
+                user_10 = user_10_id
+
+            else:
+                user_10 = user_10.name
+
+        embed: discord.Embed = discord.Embed(
+            title="Vote Leaderboard",
+            description=f"Bellow are the top 10 people\n\n:first_place: {user_1} - {vote_1}\n:second_place: {user_2} - {vote_2}\n:third_place: {user_3} - {vote_3}\n:medal: {user_4} - {vote_4}\n:medal: {user_5} - {vote_5}\n:medal: {user_6} - {vote_6}\n:medal: {user_7} - {vote_7}\n:medal: {user_8} - {vote_8}\n:medal: {user_9} - {vote_9}\n:medal: {user_10} - {vote_10}\n",
+            color=discord.Color.green()
+        )
+
+        conn = sqlite3.connect("vote.db")
+        c = conn.cursor()
+
+        c.execute("SELECT * FROM vote")
+
+        items = c.fetchall()
+        none = str(items)
+
+        if none == "[]":
+            pass
+    
+        else:
+            for item in items:
+                user_id = int(item[0])
+                votes = int(item[1])
+
+                c.execute(f"""UPDATE vote SET votes=0
+                            WHERE user_ID={user_id}
+                """)
+
+                conn.commit()
+
+            conn.close()
+
+        await votechannel.send(embed=embed)
+
+    else    :
+        votechannel = client.get_channel(858437766479609876)
+
+        await votechannel.send(f"Did not do task today as it is not the first of the month!\n\nCurrect Date - {m_d_y}")
+    
+@check_data.before_loop
+async def before_some_task():
+    await client.wait_until_ready()
+
 #economy commands
 @client.command()
 @commands.cooldown(1, 300, commands.BucketType.user)
