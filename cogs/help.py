417b94789e31e5a97416895df5b9a7be5707f45c
LegosAndStuff-Dev <masonnorberg@midco.net>
2022-03-14 18:49:46 -0500

    removed moderation part of help

--- cogs/help.py
+++ cogs/help.py
@@ -16,7 +16,6 @@ class Help(commands.Cog):
             embed.add_field(name="**Game Help**", value="If you do **d/help game** you can find out all the games that we have.", inline=False)
             embed.add_field(name="**Utility Help**", value="If you do **d/help utility** it will give you all the utility commands that we have.", inline=False)
             embed.add_field(name="**Economy Help**", value="If you do **d/help economy** is will give all the economy commands that we have.", inline=False)
-            embed.add_field(name="**Moderation Help**", value="If you do **d/help moderation** or **d/help mod** it will show you the 3 subcategories for all the moderation comamnds", inline=False)
             embed.add_field(name="**Support Links**", value="[Support Server](https://discord.gg/KxPuFvazuF)\n[Invite The Bot](https://discord.com/api/oauth2/authorize?client_id=840025172861386762&permissions=2683662023&scope=bot)\n[top.gg Profile](https://top.gg/bot/840025172861386762)")
 
             await ctx.send(embed=embed)
@@ -69,66 +68,6 @@ class Help(commands.Cog):
             #embed.add_field(name="**Gamble Help**", value="If you do **d/gamble help** it will give you the gamble commands that we have at the moment.")
 
             await ctx.send(embed=embed)
-
-        elif arg1 == "moderation" or arg1 == "mod":
-            if arg2 == None:
-                embed: discord.Embed = discord.Embed(
-                    title="**Moderation Help**",
-                    description="Bellow are all the Moderation commands that we have.",
-                    color=discord.Color.green()
-                )
-                embed.add_field(name="**Set Up**", value="If you do **d/help moderation set** it will show you all the moderation set up commands")
-                embed.add_field(name="**Commands**", value="If you do **d/help moderation commands** it will show you all the moederation commands that we have at the momment")
-                embed.add_field(name="**Mute**", value="If you do **d/help moderation search** it will show all the search moderation commands that we have.")
-
-
-                await ctx.send(embed=embed)
-            if arg2 == "set":
-                embed: discord.Embed = discord.Embed(
-                    title="**Moderation Help Setup**",
-                    description="Bellow are all the Moderation Set Up comands that we have.",
-                    color=discord.Color.green()
-                )
-                embed.add_field(name="**Moderation Set Up**", value="If you do **d/moderation start** it will allow you set up the diffrent moderation commands that we have. Look above for all the diffrent commands.\nPerms - Server Admin", inline=False)
-                embed.add_field(name="**Moderation Info**", value="If you do **d/moderation info** this will allow you to see what roles have each perms.\nPerms - Server Admin", inline=False)
-                embed.add_field(name="**Moderation Update**", value="If you do **d/moderation update [command]** it will allow you to update each of the moderation commands.\nPerms - Sever Admin", inline=False)
-
-                await ctx.send(embed=embed)
-
-            elif arg2 == "command" or arg2 == "commands":
-                embed: discord.Embed = discord.Embed(
-                    title="**Moderation Help Commands**",
-                    description="Bellow are all the Moderation commands that we have.",
-                    color=discord.Color.green()
-                )
-                embed.add_field(name="**Warn**", value="If you do **d/warn [user] [reason]** it will warn the user.\nPerms - Warn +", inline=False)
-                embed.add_field(name="**Tempmute**", value="If you do **d/tempmute [user] [time] [reason]** it will tempmute the user for the time you said.\nPerms - Tempmute +", inline=False)
-                embed.add_field(name="**Mute**", value="If you do **d/mute [user] [reason]** it will mute the user for ever if you don't unmute them.\nPerms - Mute +", inline=False)
-                embed.add_field(name="**Unmute**", value="If you do **d/unmute [user]** it will unmute the user you said.\nPerms - Unmute +", inline=False)
-                embed.add_field(name="**Kick**", value="If you do **d/kick [user] [reason]** it will kick the user from the server for that reason.\nPerms - Kick +", inline=False)
-                embed.add_field(name="**Delete Infraction**", value="If you do **d/delete [case ID]** it will allow you to delete that infraction form the database. It will never come back.\nPerms - Delete Inf +")
-                embed.add_field(name="**Ban**", value="If you do **d/ban [user] [reason]** it will ban the user from the server.\nPerms - Ban +", inline=False)
-                embed.add_field(name="**Unban**", value="If you do **d/unban [user]** it will let you unban a user from the discord server. Note you have to do something like LegosAndStuff#0501 not a user ID.\nPerms - Unban +")
-
-                await ctx.send(embed=embed)
-
-            elif arg2 == "search":
-                embed: discord.Embed = discord.Embed(
-                    title="**Moderation Help Search**",
-                    description="Bellow are all the Moderation Search commands that we have",
-                    color=discord.Color.green()
-                )
-                embed.add_field(name="**Mod Search**", value="If you do **d/mod [optional user]** it will search all for moderation commands that that use has done.\nPerms - Warn +", inline=False)
-                embed.add_field(name="**User Search**", value="If you do **d/user [optional user]** it will search for infractions that the users has been givin.\nPerms - Warn +", inline=False)
-                embed.add_field(name="**Case**", value="If you do **d/case [case ID]** it will give you the infomation of that case.\nPerms - Warn +", inline=False)
-                embed.add_field(name="**Search All**", value="If you do **d/search all** it will show the 5 most recent infractions.\nPerms - Warn +", inline=False)
-                embed.add_field(name="**Recent**", value="If you do **d/search recent** it will give you the infomation of the most recent infraction.\nPerms - Warn +", inline=False)
-                embed.add_field(name="**Delete**", value="If you do **d/delete [case ID]** it will allow you to delete a case\nPerms - Delete inf +")
-
-                await ctx.send(embed=embed)
-
-
-    
 
 def setup(client):
 	client.add_cog(Help(client))   
