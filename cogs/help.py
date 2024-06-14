d0a48f881d19070e86c5dcc5bcbea6a1806c088e
LegosAndStuff <masonnorberg@midco.net>
2024-06-14 10:30:36 -0500

    updated code

--- cogs/help.py
+++ cogs/help.py
@@ -24,7 +24,7 @@ class Help(commands.Cog):
             embed.add_field(name="**Game Help**", value="**d/help game** - info about the game commands.", inline=False)
             embed.add_field(name="**Utility Help**", value="**d/help utility** - info about utility commands.", inline=False)
             embed.add_field(name="**Economy Help**", value="**d/help economy** - info about economy commands", inline=False)
-            embed.add_field(name="**Moderation Help**", value="**d/help moderation** or **d/help mod** - shows 3 sub-categories of moderation commands", inline=False)
+            #embed.add_field(name="**Moderation Help**", value="**d/help moderation** or **d/help mod** - shows 3 sub-categories of moderation commands", inline=False)
             embed.add_field(name="**Support Links**", value="[Support Server](https://discord.gg/KxPuFvazuF)\n[Invite The Bot](https://discord.com/api/oauth2/authorize?client_id=840025172861386762&permissions=2683662023&scope=bot)\n[top.gg Profile](https://top.gg/bot/840025172861386762)")
 
             await ctx.send(embed=embed, suppress_embeds=False)
@@ -80,6 +80,10 @@ class Help(commands.Cog):
             #embed.add_field(name="**Gamble Help**", value="If you do **d/gamble help** it will give you the gamble commands that we have at the moment.")
 
             await ctx.send(embed=embed)
+
+        elif arg1 == "mod" or arg1 == "moderation":
+            await ctx.send(":construction: Coming Soon! :construction:")
+
 
         @app_commands.commands(name="ping", description="you can see what the ping is of the bot")
         @app_commands.guilds(discord.Object(840354954074128405))
