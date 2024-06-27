8a0e6be6be5f07a4f7ee0d1608f2a494f41e72f9
LegosAndStuff <masonnorberg@midco.net>
2024-06-27 07:06:27 -0500

    getting ready to work on pet update

--- cogs/pet.py
+++ cogs/pet.py
@@ -7,7 +7,6 @@
 import discord
 import os
 from discord.ext import commands
-from discord import app_commands
 from discord.ext.commands import BucketType
 from Disecon import *
 import asyncio
@@ -15,9 +14,10 @@ import random
 import math
 import sqlite3
 
-class Pet_Slash(commands.Cog):
+class Pet(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+
 async def setup(client):
-	await client.add_cog(Pet_Slash(client))
+	await client.add_cog(Pet(client))
